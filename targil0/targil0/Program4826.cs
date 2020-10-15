3e6c860a99222cb0e6805fa45baf7302df5816d8
saar taler <taler@g.jct.ac.il>
2020-10-16 01:18:10 +0300

    2 welcome functions

---
+++ targil0/targil0/Program4826.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome4826();
+            welcome8139();
+            Console.ReadKey();
+        }
+        static partial void welcome8139();
+        private static void welcome4826()
+        {
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0} welcome to my first console application", name);
+        }
+    }
+}
