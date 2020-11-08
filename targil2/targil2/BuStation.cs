2b8ff1e2b9e554b84d5a6b6e6fc0343e6c270b0c
saar taler <taler@g.jct.ac.il>
2020-11-08 21:12:03 +0200

    gets, sets, and constractors have been added to class BuStation

--- targil2/targil2/BuStation.cs
+++ targil2/targil2/BuStation.cs
@@ -13,9 +13,67 @@ namespace targil2
         double Latitude = new double();
         double Longitude = new double();
         string Address;
+        public string GSID //GS for GetSet
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+        public double GSLatitude //GS for GetSet
+        {
+            get
+            {
+                return Latitude;
+            }
+            set
+            {
+                Latitude = value;
+            }
+        }
+        public double GSLongitude //GS for GetSet
+        {
+            get
+            {
+                return Longitude;
+            }
+            set
+            {
+                Longitude = value;
+            }
+        }
+        public string GSAddress //GS for GetSet
+        {
+            get
+            {
+                return Address;
+            }
+            set
+            {
+                Address = value;
+            }
+        }
         public override string ToString()
         {
             return "Bus station code: " + ID + ", " + Latitude + "°N " + Longitude + "°E";
         }
+        public BuStation(string newID, double newLatitude, double newLongitude, string newAddress)
+        {
+            ID = newID;
+            Latitude = newLatitude;
+            Longitude = newLongitude;
+            Address = newAddress;
+        }
+        public BuStation()
+        {
+            Random r = new Random();
+            Latitude = 31 + (r.NextDouble() * (33.3-31));
+            Longitude = 34.3 + (r.NextDouble() * (35.5 - 34.3));
+        }
+
     }
 }
