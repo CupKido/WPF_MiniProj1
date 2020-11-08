11296e9adad22ef3cd2c9acbdec1b2b1ed0c2f48
saar taler <taler@g.jct.ac.il>
2020-11-08 20:23:23 +0200

    BuStation class has been created created ToString() override created

---
+++ targil2/targil2/BuStation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil2
+{
+    class BuStation
+    {
+        string ID = null;
+        double Latitude = new double();
+        double Longitude = new double();
+        string Address;
+        public override string ToString()
+        {
+            return "Bus station code: " + ID + ", " + Latitude + "°N " + Longitude + "°E";
+        }
+    }
+}
