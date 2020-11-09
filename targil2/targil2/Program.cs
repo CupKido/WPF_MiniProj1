fc3c2269defb7e9897604ccc6855a7bf34324dfd
saar taler <taler@g.jct.ac.il>
2020-11-09 22:55:09 +0200

    untill 3.6 on tagil 2

--- targil2/targil2/Program.cs
+++ targil2/targil2/Program.cs
@@ -10,8 +10,17 @@ namespace targil2
     {
         static void Main(string[] args)
         {
-            BuStationLine e = new BuStationLine();
-            e.GSStation.GSID = "3";
+           BusLine e = new BusLine();
+            e.add(new BuStationLine(new BuStation("1111", 0, 0, null), 5, 34));
+            e.add(new BuStationLine(new BuStation("2222", 0, 0, null), 4,76));
+            e.add(new BuStationLine(new BuStation("3333", 0, 0, null), 8, 23));
+            e.add(new BuStationLine(new BuStation("4444", 0, 0, null), 3, 54));
+            e.add(new BuStationLine(new BuStation("5555", 0, 0, null), 9, 38));
+           
+            BusLine newline = e.SubLine(2, 5);
+            Console.WriteLine(e.DistanceB2(3, 4));
+            Console.WriteLine(newline.DistanceB2(2, 3));
+            Console.ReadKey();
         }
     }
 }
