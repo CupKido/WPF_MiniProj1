5e00194f9f063b13dbf69487febc35f719cef559
saar taler <taler@g.jct.ac.il>
2020-11-10 21:33:39 +0200

    sort Lines by Time added

--- targil2/targil2/BLines.cs
+++ targil2/targil2/BLines.cs
@@ -9,18 +9,18 @@ using System.Threading.Tasks;
 
 namespace targil2 
 {
-    class BLines : IEnumerable
+    class BLines : IEnumerable, IComparable
     {
-        BusLine[] buslines = new BusLine[0];
+        ArrayList buslines = new ArrayList();
         public BLines()
         {
-            buslines = new BusLine[0];
+            ArrayList buslines = new ArrayList();
         }
         public IEnumerator GetEnumerator()
         {
             return buslines.GetEnumerator();
         }
-        public BusLine[] GSbuslines
+        public ArrayList GSbuslines
         {
             get
             {
@@ -69,7 +69,8 @@ namespace targil2
         }
         public int AddLine(BusLine line)
         {
-            if(buslines.Length != 0)
+            
+            if(buslines.Count != 0)
             {
                 int legit = LegitForAdd(line);
                 if (legit != 1)
@@ -77,16 +78,16 @@ namespace targil2
                     return legit;
                 }    
             }
-
-            BusLine[] temp = new BusLine[buslines.Length + 1];
-            int i = 0;
-            foreach(BusLine bus in buslines)
-            {
-                temp[i] = bus;
-                i++;
-            }
-            temp[i] = line;
-            buslines = temp;
+            buslines.Add(line);
+            //BusLine[] temp = new BusLine[buslines.Length + 1];
+            //int i = 0;
+            //foreach(BusLine bus in buslines)
+            //{
+            //    temp[i] = bus;
+            //    i++;
+            //}
+            //temp[i] = line;
+            //buslines = temp;
             return 0;
         }
         public void RemoveLine(BusLine line)
@@ -94,19 +95,22 @@ namespace targil2
             int location = searchLine(line);
             if(location == -1)
             { return; }
-            BusLine[] temp = new BusLine[buslines.Length-1];
-            int i = 0;
-            int j = 0;
-            foreach(BusLine busLine in buslines)
-            {
-                if(i != location)
-                {
-                    temp[j] = busLine;
-                    j += 1;
-                }
-                i += 1;
-            }
-            buslines = temp;
+
+            buslines.RemoveAt(location);
+
+            //BusLine[] temp = new BusLine[buslines.Length-1];
+            //int i = 0;
+            //int j = 0;
+            //foreach(BusLine busLine in buslines)
+            //{
+            //    if(i != location)
+            //    {
+            //        temp[j] = busLine;
+            //        j += 1;
+            //    }
+            //    i += 1;
+            //}
+            //buslines = temp;
         }
         public BLines PAS(BuStation stat) // passes in stations
         {
@@ -125,7 +129,10 @@ namespace targil2
         }
         public BLines sortbytime()
         {
-            BLines temp = buslines.sort();
+            BLines n = new BLines();
+            n.buslines = buslines;
+            n.buslines.Sort();
+            return n;
         }
     }
 }
