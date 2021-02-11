53b57396e9e099e984404f5e895c96e0fb7f9b6e
saar taler <taler@g.jct.ac.il>
2021-02-11 14:56:50 +0200

    more pl and pull

---
+++ Final/PL.WPF/ShowBusInfo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BL;
+
+namespace PL.WPF
+{
+    /// <summary>
+    /// Interaction logic for ShowBusInfo.xaml
+    /// </summary>
+    public partial class ShowBusInfo : Window
+    {
+        MainWindow Main;
+        BO.BUS ThisBus;
+        public ShowBusInfo(BO.BUS bus, MainWindow main)
+        {
+            InitializeComponent();
+            Main = main;
+            ThisBus = bus;
+        }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            addBusWindow win = new addBusWindow(ThisBus.LicenseNum, Main);
+            win.Show();
+            this.Close();
+        }
+    }
+}
